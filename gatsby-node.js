298788ee93f59d0a4ed934f78b1c05eaaae5702c
Gerald <gera2ld@163.com>
2020-06-13 01:20:03 +0800

    fix: commonjs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
 // gatsby-preset-babel enables loose mode by default,
 // d3 does not work in loose mode, so we have to external it
 // and load it from CDN.
-exports function onCreateWebpackConfig({ actions }) {
+exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     externals: {
       d3: 'd3',
     },
   });
-}
+};
